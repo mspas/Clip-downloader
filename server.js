711393361a98d1ce6dbb6837b751658f7c3f1b57
Marcin Spasinski <marcin7789@gmail.com>
2020-06-04 18:21:59 +0200

    keys fix4

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const twitchOptions = {
   headers: {
     Accept: "application/vnd.twitchtv.v5+json",
-    "Client-ID": "uywdn3u5k0i0p27xlasmsp7vxk1o2t",
+    "Client-ID": "",
   },
   method: "GET",
 };
 
-const YT_API_KEY = "AIzaSyC6po_hBpCzAlmDcGBG8lqKBo0SM-B9cyw";
+const YT_API_KEY = "";
 
 app.get("/api/download/twitchclip", async (req, res) => {
   var clipName = req.query.videoId;
